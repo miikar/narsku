e2333d68df18e3991f4d8b7fe01117532b329196
Miika Rouvinen <miika.j.rouvinen@gmail.com>
2016-04-08 14:40:26 +0300

    Test code for calibrating

--- app/app.ino
+++ app/app.ino
@@ -1,11 +1,36 @@
 void setup() {
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  
+
+  static int minValue = 1024;
+  static int maxValue = 0;
   int sensorValue = analogRead(A0);
 
-  Serial.println(sensorValue);
-  delay(1);
+  // Calibrate
+  if(sensorValue > maxValue) {
+    maxValue = sensorValue;  
+  }
+  if(sensorValue < minValue) {
+    minValue = sensorValue;  
+  }
+
+  int threshold = 0.5*(maxValue - minValue) + minValue;
+
+  Serial.print(sensorValue);
+  Serial.print(" ");
+  Serial.print(minValue);
+  Serial.print(" ");
+  Serial.print(maxValue);
+  Serial.print(" ");
+  Serial.println(threshold);
+
+  if(sensorValue >= threshold) {
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13, LOW);
+  }
+  delay(100);
 }
